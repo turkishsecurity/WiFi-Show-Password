3d224721df9b0c46051bc79f9c7ebed37b1199e7
Xale Security <81574241+xaletr@users.noreply.github.com>
2021-12-03 21:49:50 +0300

    Update wifikir.py

--- wifikir.py
+++ wifikir.py
@@ -12,20 +12,21 @@ CODED BY XALE ~ GitHub: @xaletr
 """
 
 if sys.argv[1] in ['-w', '--wifi']:
-	wifi = sys.argv[2]
-print(banner)
-print(" ")
-print(" ")
-print("Kırılabilinecek Wi-Filer : ")
-print(" ")
-print(" ")
-os.system('netsh wlan show profiles')
-print(" ")
-print(" ")
-print(" ")
-time.sleep(3)
-os.system('netsh wlan show profiles "'+wifi+'" key=clear')
-print(" ")
-print(" ")
-print(" ")
-print("Şifre Key Content Bölümünde")
+ wifi = sys.argv[2]
+ print(banner)
+ print(" ")
+ print(" ")
+ time.sleep(3)
+ os.system('netsh wlan show profiles "'+wifi+'" key=clear')
+ print(" ")
+ print(" ")
+ print(" ")
+ print("Şifre Key Content Bölümünde")
+
+elif sys.argv[1] in ['--networks', '-n']:
+ print(" ")
+ print(" ")
+ os.system('netsh wlan show profiles')
+ print(" ")
+ print(" ")
+ print(" ")
